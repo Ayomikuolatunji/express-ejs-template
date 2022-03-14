0c927124dd08a18213cbd127898487c1114d0ee4
ayomikuolatunji <ayomikuolatunji@gmail.com>
2022-03-14 04:51:40 +0100

    reading files and writing files dynamically

--- controllers/product.js
+++ controllers/product.js
@@ -11,8 +11,7 @@ const getAllproducts=(req, res, next) => {
 }
 
 const allProducts=(req, res, next) => {
-    const products=new Products()
-    products.fetchAll()
+    const products=Products.fetchAll()
     res.status(200).render("shop",{
         prod:products, title:"My Shopping",prodTitle:"Great Books",path:"/"
     })
--- models/product.js
+++ models/product.js
@@ -1,14 +1,30 @@
 
-const products=[]
+const fs=require("fs");
+const path=require("path")
+const pathDir=require("../util/path")
 
 class Products{
-    constructor(){
+    constructor(title,price){
         this.title=title,
         this.price=price
     }
 
     save(){
-        product.push(this)
+        const p = path.join(
+             pathDir,
+            'data',
+            'products.json'
+          );
+          fs.readFile(p, (err, fileContent) => {
+            let products = [];
+            if (!err) {
+              products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), err => {
+              console.log(err);
+            });
+          });
     }
 
      static fetchAll(){
