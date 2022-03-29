251cdfd7390b9e70d790f43c97fe17db996d3193
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-03-29 02:37:36 +0100

    sequilize connection

--- models/product.js
+++ models/product.js
@@ -1,2 +1,29 @@
 const Sequelize=require("sequelize");
 const sequelize=require("../db/database")
+
+sequelize.define("products",{
+   id:{
+     type:Sequelize.INTEGER,
+     autoIncrement:true,
+     allowNull:false,
+     primary_key:true
+   },
+   title:{
+     type:Sequelize.STRING,
+     allowNull:false
+   },
+   price:{
+     type:Sequelize.DOUBLE,
+     allowNull:false,
+   },
+   imageUrl:{
+     type:Sequelize.STRING,
+     allowNull:false
+   },
+   description:{
+     type:Sequelize.STRING,
+     allowNull:false
+   }
+})
+
+module.exports=sequelize
