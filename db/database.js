7047eb44aacc1da3c7d2c0c411395b04ff34300d
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-03-29 02:31:22 +0100

    sequilize connection

--- db/database.js
+++ db/database.js
@@ -1,10 +1,8 @@
-const mysql=require("mysql2")
+const Sequelize=require("sequelize");
 
-const pool=mysql.createPool({
+const sequelize=new Sequelize("node-sql","root","ayoscript222",{
     host:"localhost",
-    user:"root",
-    database:"node-sql",
-    password:"ayoscript222"
-})
+    dialect:"mysql"
+});
 
-module.exports=pool.promise()
+module.exports=sequelize
