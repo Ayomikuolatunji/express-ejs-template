3de22b39a46da43d5805b1d225e0e60c9bbbf419
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-01 01:12:34 +0100

    end

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,4 @@
-const Products = require('../models/product');
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -13,20 +13,21 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Products.create({
-      title:title,
-      price:price,
-      imageUrl:imageUrl,
-      description:description,
-      userId:req.user.id
-  })
-  .then(data=>{
-    console.log("created sucessfully")
-      return res.redirect("/admin/products")
-  })
-  .catch(err=>{
-    console.log(err)
-  })
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description
+    })
+    .then(result => {
+      // console.log(result);
+      console.log('Created Product');
+      res.redirect('/admin/products');
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -35,19 +36,22 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Products.findOne({where:{id:prodId}}).then(product=>{
-    if (!product) {
-      return res.redirect('/');
-    }
-      return res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
-      editing: editMode,
-      product: product
-    });
-  }).catch(err=>{
-    console.log(err)
-  });
+  req.user
+    .getProducts({ where: { id: prodId } })
+    // Product.findById(prodId)
+    .then(products => {
+      const product = products[0];
+      if (!product) {
+        return res.redirect('/');
+      }
+      res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        editing: editMode,
+        product: product
+      });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -56,45 +60,43 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-
-  Products.findOne({where:{id:prodId}})
-  .then(product=>{
-    product.title=updatedTitle;
-    product.price=updatedPrice;
-    product.imageUrl=updatedImageUrl;
-    product.description=updatedDesc;
-    return product.save()
-  })
-  .then(dir=>{
-    res.redirect('/admin/products');
-  })
-  .catch(err=>{
-    console.log(err)
-  })
-
+  Product.findOne({where:{id:prodId}})
+    .then(product => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.description = updatedDesc;
+      product.imageUrl = updatedImageUrl;
+      return product.save();
+    })
+    .then(result => {
+      console.log('UPDATED PRODUCT!');
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-  Products.findAll().then(product=>{
-    res.render('admin/products', {
-      prods: product,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
-    });
-    }).catch(err=>{
-        console.log(err)
+  req.user
+    .getProducts()
+    .then(products => {
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products'
+      });
     })
+    .catch(err => console.log(err));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Products.findOne({where:{id:prodId}}).then(product=>{
-       return product.destroy()
-  })
-  .then(red=>{
-    res.redirect('/admin/products');
-  })
-  .catch(err=>{
-     console.log(errs)
-  })
+  Product.findOne({where:{id:prodId}})
+    .then(product => {
+      return product.destroy();
+    })
+    .then(result => {
+      console.log('DESTROYED PRODUCT');
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err));
 };
--- models/user.js
+++ models/user.js
@@ -1,29 +1,16 @@
 const Sequelize = require('sequelize');
 
-const sequelize = require('../db/database');
+const sequelize = require('../util/database');
 
-const User = sequelize.define('user-profile', {
+const User = sequelize.define('user', {
   id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
     allowNull: false,
     primaryKey: true
   },
-  name: {
-      type:Sequelize.STRING,
-      validate:{
-        isAlphanumeric: true, 
-        len: [2,],  
-      },
-      allowNull:false
-  },
-  email:{
-      type:Sequelize.STRING,
-      validate:{
-        isEmail: true,   
-      },
-      allowNull:false
-  }
+  name: Sequelize.STRING,
+  email: Sequelize.STRING
 });
 
-module.exports = User
+module.exports = User;
